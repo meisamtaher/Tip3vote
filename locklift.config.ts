adea84f1601511297bd05bac511d8b64553ad8bc
sam <sam@sams-MacBook-Air.local>
2023-05-22 18:20:23 +0330

    intial commit ( with error on importing sol file)

---
+++ locklift.config.ts
@@ -0,0 +1,97 @@
+import { LockliftConfig } from "locklift";
+import { FactorySource } from "./build/factorySource";
+
+
+declare global {
+const locklift: import("locklift").Locklift<FactorySource>;
+  }
+
+  const LOCAL_NETWORK_ENDPOINT = "http://localhost/graphql";
+  
+  const VENOM_TESTNET_ENDPOINT = process.env.VENOM_TESTNET_ENDPOINT || "https://jrpc-testnet.venom.foundation/rpc";
+  const VENOM_TESTNET_TRACE_ENDPOINT =
+  process.env.VENOM_TESTNET_TRACE_ENDPOINT || "https://gql-testnet.venom.foundation/graphql";
+  
+
+  const config: LockliftConfig = {
+  compiler: {
+  // Specify path to your TON-Solidity-Compiler
+  // path: "/mnt/o/projects/broxus/TON-Solidity-Compiler/build/solc/solc",
+
+  // Or specify version of compiler
+  version: "0.59.0",
+
+  
+  externalContracts: {
+  "node_modules/tip3/build": ["TokenRoot", "TokenWallet"]
+  },
+  
+  },
+  linker: {
+  // Specify path to your stdlib
+  // lib: "/mnt/o/projects/broxus/TON-Solidity-Compiler/lib/stdlib_sol.tvm",
+  // // Specify path to your Linker
+  // path: "/mnt/o/projects/broxus/TVM-linker/target/release/tvm_linker",
+
+  // Or specify version of linker
+  version: "0.15.48",
+  },
+  networks: {
+  local: {
+  // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
+  connection: {
+  id: 1,
+  group: "localnet",
+  type: "graphql",
+  data: {
+  endpoints: [LOCAL_NETWORK_ENDPOINT],
+  latencyDetectionInterval: 1000,
+  local: true,
+  },
+  },
+  // This giver is default local-node giverV2
+  giver: {
+  // Check if you need provide custom giver
+  address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
+  key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
+  },
+  tracing: {
+  endpoint: LOCAL_NETWORK_ENDPOINT,
+  },
+  keys: {
+  // Use everdev to generate your phrase
+  // !!! Never commit it in your repos !!!
+  // phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
+  amount: 20,
+  },
+  }, 
+  test: {
+  connection:  {
+  id: 1000,
+  type: "jrpc",
+  group: "dev",
+  data: {
+  endpoint: VENOM_TESTNET_ENDPOINT,
+  },
+  },
+  giver: {
+  address: "0:cf903ae7045c5553df013a1f48e8dabaddd39671da24cd695b3757cfd65c2bc4",
+  key: "0000000000000000000000000000000000000000000000000000000000000000",
+  },
+  tracing: {
+  endpoint: VENOM_TESTNET_TRACE_ENDPOINT, 
+  },
+  keys: {
+  // Use everdev to generate your phrase
+  // !!! Never commit it in your repos !!!
+  phrase: "phrase",
+  amount: 20,
+  },
+  },
+  },
+  mocha: {
+  timeout: 2000000,
+  },
+  };
+
+  export default config;
